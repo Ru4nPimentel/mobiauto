20b88d6c503c1af3065012c37a5fec77982e1a54
Ruan Souza <ruanpimentel.job@gmail.com>
2023-10-30 19:54:55 -0300

    feat: test

---
+++ jest.config.mjs
@@ -0,0 +1,12 @@
+import nextJest from 'next/jest.js'
+ 
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config = {
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',  
+}
+
+export default createJestConfig(config)
